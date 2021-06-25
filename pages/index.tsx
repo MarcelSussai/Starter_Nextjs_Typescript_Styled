c4fedbdcc30edd0690fc9f0e2724edc8b8c8c3c3
MarcelSussai <marcelsussaideoliveira@hotmail.com>
2021-06-25 02:15:40 -0300

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
-import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import Head from '../src/components/head'
 
-export default function Home() {
-  return <Title>My page</Title>
+
+const Home = () => {
+  return (
+  <>
+    <Head title="Home" />
+    <h1>Teste</h1>
+  </>
+);
+
 }
+
+export default Home;
